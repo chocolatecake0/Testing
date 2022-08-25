965c70dcc08cfa427a26db99200eb73c6314afc8
chocolatecake0 <eren531@windowslive.com>
2022-08-26 01:03:22 +0300

    works on click

--- test/script.js
+++ test/script.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
-    const header = $("#header")
-    console.log(header)
-    const x = header
-    console.log(x)
-    for (let i=0;i<100;i++){
-        x.clone().appendTo("body");
-    }
+    $("#header").click(function(){
+        const header = $("#header")
+        console.log(header)
+        const x = header
+        console.log(x)
+        for (let i=0;i<100;i++){
+            x.clone().appendTo("body");
+        }
+    })
+
 })
