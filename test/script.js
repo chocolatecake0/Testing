c78ab07fd3051cf05f599d80cd4c158d8887fd39
chocolatecake0 <eren531@windowslive.com>
2022-08-26 12:09:16 +0300

    yes

--- test/script.js
+++ test/script.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
         const header = $("#header")
         console.log(header)
         const x = header
-        console.log(x)
         for (let i=0;i<100;i++){
             x.clone().appendTo("body");
         }
