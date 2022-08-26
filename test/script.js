cdb5ae766cf1b06b4103f564f96cb814ec0187f1
chocolatecake0 <eren531@windowslive.com>
2022-08-26 23:59:44 +0300

    yes

--- test/script.js
+++ test/script.js
@@ -7,5 +7,20 @@ $(document).ready(function(){
             x.clone().appendTo("body");
         }
     })
+    function control(e) {
+        if(e.keyCode === 37) {
+            moveLeft()
+        }
+        else if(e.keyCode === 38) {
+            rotate()
+        }
+        else if(e.keyCode === 39) {
+           moveRight()            
+        }   
+        else if(e.keyCode === 40) {
+           moveDown()
+        }       
+    }
+    document.addEventListener('keyup', control)
 
 })
